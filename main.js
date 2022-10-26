cbb3ddd610737045528ac10a3703a2b7da318cd0
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-10-26 17:12:19 +0200

    folders

---
+++ main.js
@@ -0,0 +1,21 @@
+//let seriesPending = ['The Crown', 'Sandman', 'Knightfall', 'Dark', 'Bodyguard', 'Troya']
+//console.log(seriesPending);
+
+//seriesPending.push('Dr House')
+//console.log(seriesPending);
+
+
+let seriesPending = ["The crown", "Sandman", "Knightfall", "Dark", "Shark", "Bodyguard", "Troya", "boolean99"]
+//for (let i = 0; i < seriesPending.length; i++) {
+//    console.log(seriesPending, seriesPending.indexOf(''));
+//}
+
+//if (seriesPending = "boolean99") {
+//    console.log(seriesPending.includes("boolean99"), "found it")
+//}
+
+let seriesWatching = []
+
+
+
+
